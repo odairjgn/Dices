79b9e4aa8d6421eff58b17d44410406cea0c98d3
Odair José Gambert Nossa <odairjgn@outlook.com>
2017-04-14 21:16:34 -0300

    Removido referencias

--- Dices/DicesCustomControls/Componentes/HtmlEditor.cs
+++ Dices/DicesCustomControls/Componentes/HtmlEditor.cs
@@ -1,20 +1,12 @@
 ﻿using System.Windows.Forms;
-using LiveSwitch.TextControl;
-
 namespace DicesCustomControls.Componentes
 {
     public partial class HtmlEditor : UserControl
     {
-        private Editor _editor;
-
         public HtmlEditor()
         {
             CheckForIllegalCrossThreadCalls = false;
             InitializeComponent();
-
-            _editor = new Editor();
-            _editor.Dock = DockStyle.Fill;
-            Controls.Add(_editor);
         }
     }
 }
